1659e86f38d49b0b6d5b997d992b250f93010751
yusrilmahendri <yusrilmahendri.yusril@gmail.com>
2026-05-17 14:28:50 +0700

    handling error

--- app/Models/category.php
+++ app/Models/category.php
@@ -2,14 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Saldo;
 
 class Category extends Model
 {
-     protected $fillable = ['id', 'name'];
+    use HasFactory;
 
-    public function saldos(){
+    protected $fillable = ['id', 'name'];
+    protected $table = 'categories';
+
+    public function saldos()
+    {
         return $this->hasMany(Saldo::class);
     }
 }
